1a4b140fbfa213c30d346e8d609aa3f4a97d25fb
Benjamin Siret <benjaminsiret@outlook.fr>
2021-03-03 15:07:06 +0100

    add style

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,4 @@ class Review < ApplicationRecord
   belongs_to :restaurant
   validates :content, :rating, presence: true
   validates :rating, inclusion: { in: 0..5 }, numericality: { only_integer: true }
-
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+2.times do
+  Restaurant.create(
+    name: Faker::Restaurant.name,
+    category: 'chinese',
+    address: Faker::Address.street_address
+  )
+
+end
+
+2.times do
+  Restaurant.create(
+    name: Faker::Restaurant.name,
+    category: 'french',
+    address: Faker::Address.street_address
+  )
+end
+
+Restaurant.create(
+  name: Faker::Restaurant.name,
+  category: 'italian',
+  address: Faker::Address.street_address
+)
+
+Restaurant.create(
+  name: Faker::Restaurant.name,
+  category: 'japanese',
+  address: Faker::Address.street_address
+)
